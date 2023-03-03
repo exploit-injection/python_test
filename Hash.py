9e270d2b7c3772eccb19d4e0551d17567f9c982c
Polina Sergacheva <malaya160901@mail.ru>
2023-03-03 22:16:44 +0700

    Проверка хеша текстового файла

---
+++ Hash.py
@@ -0,0 +1,19 @@
+import hashlib
+
+
+# Функция для получения хеш-суммы файла
+def hash_file(file_name):
+    h = hashlib.sha1()  # Создание объекта хеша
+
+    #  Открытие файла для чтения в двоичном режиме
+    with open(file_name, 'rb') as file:
+        chunk = 0
+        while chunk != b'':
+            chunk = file.read(1024)  # Циклом читаем до конца файла только 1024 байта за раз
+            h.update(chunk)
+
+    return h.hexdigest()
+
+
+message = hash_file("../name.txt")
+print(message)
