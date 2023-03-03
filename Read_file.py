0adeeca6a44de31c517afca8090d1fb04c5f8f5b
Polina Sergacheva <malaya160901@mail.ru>
2023-03-03 21:30:10 +0700

    Переименование файла. Анализ файла с 14 млн паролей.

--- Read_file.py
+++ Read_file.py
@@ -1,9 +1,11 @@
 #  Создание переменной, в которой путь к файлу
-inputfile = "../name.txt"
+inputfile = "../password.txt"
 # Открытие файла. Выбор в каком режиме открыть файл. Пока только чтение
 myfile = open(inputfile, mode='r', encoding='latin_1')
 # Чтение файла, который был указан
 # print(myfile.read())
 # Вывод студентов с номерами строк по порядку, начиная с 1
 for num, line in enumerate(myfile, 1):
-    print("Student № " + str(num) + ": " + line.strip())
+    #  Вывод всех паролей, в которых есть vasya
+    if "vasya" in line:
+        print("Line № " + str(num) + ": " + line.strip())
